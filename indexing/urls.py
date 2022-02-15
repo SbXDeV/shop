9f16f7902f622cf8d39f5b32b2bbeabd120ea1b0
Lyxxar < lilmounty@vk.com>
2022-02-15 22:25:25 +0400

    v1.1

--- indexing/urls.py
+++ indexing/urls.py
@@ -6,5 +6,6 @@ app_name = 'indexing'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('contact', views.Contact.as_view(), name='contact'),
+    path('shop/', views.Shop.as_view(), name='shop'),
     path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
 ]
--- indexing/views.py
+++ indexing/views.py
@@ -3,16 +3,26 @@ from indexing.models import Product
 
 
 class Index(ListView):
+    """ Главная страница сайта """
     model = Product
     context_object_name = 'product'
     template_name = 'index/index.html'
 
 
+class Shop(ListView):
+    """ Страница магазина """
+    model = Product
+    context_object_name = 'product'
+    template_name = 'index/shop-3-column.html'
+
+
 class Contact(TemplateView):
+    """ Страница контактной формы """
     template_name = 'index/contact.html'
 
 
 class ProductDetailView(DetailView):
+    """ Страница детального просмотра товара """
     model = Product
     context_object_name = 'product'
     template_name = 'index/shop-single-product.html'
